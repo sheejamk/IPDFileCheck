df19356f191275ddc4b1304705a10780616a2e7f
Sheeja Manchira Krishnan <sheejamk@gmail.com>
2021-08-04 10:56:05 +0100

    clean

--- packDAMipd/data-raw/table_param.R
+++
@@ -1,7 +0,0 @@
-## parameter file data for testing
-file <- "data-raw/table_param.csv"
-
-table_param <- read.csv(file, stringsAsFactors = F)
-
-# Create .rda object for trial_data in data folder
-usethis::use_data(table_param, overwrite = TRUE)
--- packDAMipd/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(packDAMipd)
-
-test_check("packDAMipd")
